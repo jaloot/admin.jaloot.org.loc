0d336471e6b10f547351c20653f909c84499ffa3
Hassan ELKHAROUA <Hassan.elkharoua@gmail.com>
2026-09-13 15:07:49 +0100

    Add Middleware IP CF

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,14 @@ return Application::configure(basePath: dirname(__DIR__))
         apiPrefix: 'v2',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR
+                | Request::HEADER_X_FORWARDED_HOST
+                | Request::HEADER_X_FORWARDED_PORT
+                | Request::HEADER_X_FORWARDED_PROTO
+        );
+
         $middleware->api(append: [
             \App\Http\Middleware\AddResponseMeta::class,
         ]);
