a9732ad816bc377bf70afe1b0b3d10c47c2d201c
Hassan ELKHAROUA <Hassan.elkharoua@gmail.com>
2026-09-20 01:34:06 +0100

    Add Allowed Lang ES / NL

--- app/Http/Resources/ChapterResource.php
+++ app/Http/Resources/ChapterResource.php
@@ -9,7 +9,7 @@ use App\Models\Basmala;
 class ChapterResource extends JsonResource
 {
     private const DEFAULT_LANG = 'ar';
-    private const ALLOWED_LANGS = ['ar', 'en', 'fr'];
+    private const ALLOWED_LANGS = ['ar', 'en', 'fr' , 'es' , 'nl'];
 
     public function toArray(Request $request): array
     {
